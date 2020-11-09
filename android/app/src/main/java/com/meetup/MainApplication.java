aa9815cbf7de57cba733edb9e21d9755eedd508c
Yadav Padiyar M <yadav.padiyar@hashedin.com>
2020-11-09 22:00:21 +0530

    finished cicd  setup

--- android/app/src/main/java/com/meetup/MainApplication.java
+++ android/app/src/main/java/com/meetup/MainApplication.java
@@ -1,5 +1,6 @@
 package com.meetup;
 
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -27,6 +28,10 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(
+       			new ReactNativeConfigPackage()
+    );
+
           return packages;
         }
 
